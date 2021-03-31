08aec6932e072dfe7255f70b099e9cbd26efed58
bigyidbuilds <bigyidbuilds@gmail.com>
2021-03-31 21:55:21 +0100

    Create uservar.py

---
+++ uservar.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+class tmdbapi():
+	''' TMDB Details key obtainable from https://www.themoviedb.org/settings/api '''
+	key='50393dea0fb0eb249291a50a50ef022a'
+	username=''
+	password=''
+
+
+class youtubeapi():
+	''' Youtube details https://developers.google.com/youtube/v3/getting-started '''
+	apiKey = 'AIzaSyDAZJH9asmAAZoDZL6Gm2lzo30JbCDgVOI'
+	clientId = '634756433129-jk0hptk9htqv9k5or1j8ghgk1n9fb0lb.apps.googleusercontent.com'
+	clientSecret = 'imNME224p06Bmq0ZMOxMRSUA'
+
+
+class url():
+	''' url path to your playlist files, film and tv required are on seperate files ''' 
+	filmpath = 'https://www.dropbox.com/s/ozbzbaknug79kg9/movies.json?dl=1'
+	tvpath   = 'https://www.dropbox.com/s/7anu15cvoeisghx/tvshows.json?dl=1'
